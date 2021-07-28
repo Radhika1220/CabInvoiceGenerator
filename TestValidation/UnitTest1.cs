68017f2d64d9f053eb4e0cf30a4feb4d9d629cf5
Radhika <radhika.shankar1220@gmail.com>
2021-07-28 22:27:11 +0530

    [Radhika] calculating totalfare,avgfare for multiple rides

--- TestValidation/UnitTest1.cs
+++ TestValidation/UnitTest1.cs
@@ -82,5 +82,37 @@ namespace TestValidation
                 Assert.AreEqual(expected, ex.message);
             }
         }
+        [TestMethod]
+        public void Return_Multiple_Rides_TotalFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 55.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(summary.totalFare, expected.totalFare);
+        }
+
+        [TestMethod]
+        public void Return_Multiple_Rides_AverageFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2,55.0) ;
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(summary.avgFare, expected.avgFare);
+        }
+
+        [TestMethod]
+        public void Return_Multiple_Rides_NumofRides()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM_RIDE);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 55.0);
+            InvoiceSummary expected = invoice.CalculateFare(rides);
+            Assert.AreEqual(summary.numOfRides, expected.numOfRides);
+        }
     }
 }
